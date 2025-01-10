1102e4b61f3e949acbfa5a812768cded7f6a1285
bpicassokennedy <b3llakennedy@gmail.com>
2025-01-10 00:23:56 +0000

    erm

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int menu();
+void menu();
 string userInput(); 
 bool isPalindrome(string s);
 
 int main(){
     int choice;
     do{
-        choice = menu();
+        menu();
+        string input1 = userInput();
+        choice = stoi(input1);
+        while(choice < 1 || choice > 2){ //keep requesting user input until it's a valid integer
+            cout << "Invalid option! Try again: ";
+            input1 = userInput();
+            choice = stoi(input1);
+        }
         if(choice == 1){
-            string input = userInput();
-            bool palindrome = isPalindrome(input);
+            cout << "Enter a word to check if it's a palindrome: ";
+            string input2 = userInput();
+            bool palindrome = isPalindrome(input2);
             if(palindrome){
-                cout << input << " is a palindrome! :)" << endl << endl;
+                cout << input2 << " is a palindrome! :)" << endl << endl;
             } else{
-            cout << input << " is not a palindrome... :( " << endl << endl;
+            cout << input2 << " is not a palindrome... :( " << endl << endl;
             }
         } else{
             cout << "Goodbye! :)" <<    endl;
@@ -24,28 +32,17 @@ int main(){
     return 0;
 }
 
-int menu(){ //presents to the user after each word, phrase, or sequence is checked. giving them the option to check another word or exit
-    string input;
-    int choice;
-    cout << "--- PALINDROME CHECKER! ---" <<    endl;
+void menu(){ //presents to the user after each word, phrase, or sequence is checked. showing them their options to check another word or exit
+    cout << "--- PALINDROME CHECKER! ---" << endl;
     cout << "Palindrome Defintion: a word, phrase, or sequence that reads the same backward as forward" <<  endl;
-    cout << "----------------------------" <<   endl;
-    cout << "1. Check to see if a word is a palindrome" <<  endl;
-    cout << "2. Exit " <<   endl;
+    cout << "----------------------------" << endl;
+    cout << "1. Check to see if a word is a palindrome" << endl;
+    cout << "2. Exit " << endl;
     cout << "Enter your choice: ";
-    getline(cin, input);
-    choice = stoi(input); //converts string to int, otherwise following expression executes incorrectly
-    while(choice != 1 && choice != 2){
-        cout << "Invalid option! Try again: ";
-        getline(cin, input);
-        choice = stoi(input);
-    }
-    return choice;
 }
 
-string userInput(){ //gets user input, will eventually pass this string into isPalindrome 
+string userInput(){ //gets user input, used for getting user's choice from the menu and inputting their desired string
     string input;
-    cout << "Enter a word to check if it's a palindrome: ";
     getline(cin, input);
     return input; //returns user input as they initially entered it into the terminal
 }
